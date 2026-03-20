9e0b43ec230e175600d14fdb197bc61c85ade438
Gwladyslsn <gwladyslaisne@outlook.fr>
2026-03-20 10:28:38 +0100

    structure

---
+++ assets/js/componentsLoad.js
@@ -0,0 +1,11 @@
+async function loadHTML(id, file) {
+    const response = await fetch(file);
+    const data = await response.text();
+    document.getElementById(id).innerHTML = data;
+}
+
+
+
+
+loadHTML("header", "html/components/header.html");
+loadHTML("footer", "html/components/footer.html");
